3b8756a4ba2220a622d4eabc2b232571042f3f7d
robinmollah <robinsajin@gmail.com>
2020-03-24 21:56:58 +0600

    Phaser added | Style Changed | (c) Robin Mollah added

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,7 @@ let data = require('../src/csv-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	if(req.headers.host.indexOf('covid19') > -1 || req.headers.host.indexOf('corona') > -1) {
-		console.log(req.headers.host.indexOf('covid19'), req.headers.host.indexOf('corona'));
-		res.render('covid19/index', {data: data.fetch()});
-	} else {
-		res.render('covid19/index', {data: data.fetch()});
-	}
+	res.render('index', {data: data.fetch()});
 });
 
 router.get('/covid19', (req, res) => {
