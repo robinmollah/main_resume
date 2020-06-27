c6832fdde7faa0c2c52f7e6a8a69c43df0beeacd
robinmollah <robinsajin@gmail.com>
2020-06-27 15:15:49 +0600

    Pre-activated tab

--- public/javascripts/resume.js
+++ public/javascripts/resume.js
@@ -4,17 +4,26 @@ let education = document.getElementById("education");
 
 let tabs = [foe, experiences, education];
 let sections = document.getElementsByClassName("tab");
+
+activateTab();
+
+function activateTab(target = document.getElementById("field-of-expertise-tab")){
+	console.log("target", target);
+	let sectionId;
+	for(let otherElem of sections){
+		$(otherElem).removeClass("tab-active");
+		sectionId = $(otherElem).attr("section");
+		console.log(sectionId);
+		$("#" + sectionId).css("display", "none");
+	}
+	$(target).addClass("tab-active");
+	sectionId = $(target).attr("section");
+	$("#" + sectionId).css("display", 'block');
+}
+
+
 for(let elem of sections){
 	elem.addEventListener("click", (elem) => {
-		let sectionId;
-		for(let otherElem of sections){
-			$(otherElem).removeClass("tab-active");
-			sectionId = $(otherElem).attr("section");
-			console.log(sectionId);
-			$("#" + sectionId).css("display", "none");
-		}
-		$(elem.target).addClass("tab-active");
-		sectionId = $(elem.target).attr("section");
-		$("#" + sectionId).css("display", 'block');
+		activateTab(elem.target);
 	})
 }
