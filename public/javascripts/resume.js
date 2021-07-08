e330532715a61261bd565ddcd75878ed6c13aa07
robinmollah <robinsajin@gmail.com>
2021-07-09 04:57:09 +0600

    Skill Search | Skill viewer

--- public/javascripts/resume.js
+++ public/javascripts/resume.js
@@ -8,12 +8,10 @@ let sections = document.getElementsByClassName("tab");
 activateTab();
 
 function activateTab(target = document.getElementById("field-of-expertise-tab")){
-	console.log("target", target);
 	let sectionId;
 	for(let otherElem of sections){
 		$(otherElem).removeClass("tab-active");
 		sectionId = $(otherElem).attr("section");
-		console.log(sectionId);
 		$("#" + sectionId).css("display", "none");
 	}
 	$(target).addClass("tab-active");
