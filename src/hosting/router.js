74b98bc7c4d04e509123d798241831e26dd8938b
robinmollah <robinsajin@gmail.com>
2021-09-06 23:03:17 +0600

    Unzipping successful

--- src/hosting/router.js
+++ src/hosting/router.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const multer  = require('multer');
-const fileUpload = require('express-fileupload');
 const HOSTING_DIR = '/sites/';
 const path = require('path');
+const unzipper = require('unzipper');
+const fs = require('fs');
 /*
  * Root path: SERVER/api/hosting
  */
@@ -16,13 +16,12 @@ router.get('/index', (req, res) => {
 
 // TODO plan data recovery and portability
 router.post("/upload", (req, res) => {
-	console.log(req);
-	// if(!req.files){
-	// 	return res.status(400).send("No files were found");
-	// }
-	// if(!req.body.user_id){
-	// 	return res.status(400).send("user_id not found");
-	// }
+	if(!req.files){
+		return res.status(400).send("No files were found");
+	}
+	if(!req.body.user_id){
+		return res.status(400).send("user_id not found");
+	}
 	const file = req.files.file;
 	const extensionName = path.extname(file.name);
 
@@ -31,13 +30,15 @@ router.post("/upload", (req, res) => {
 	}
 
 	const user_id = req.body.user_id;
-	const location = __dirname + HOSTING_DIR + user_id + ".zip";
+	const directory = __dirname + HOSTING_DIR + user_id;
+	const location = directory + ".zip";
 
 	file.mv(location, (err) => {
 		if(err){
 			return res.status(500).send(err);
 		}
-
+		fs.createReadStream(location)
+			.pipe(unzipper.Extract({ path: directory }));
 		return res.send({ status: "true", path: location });
 	})
 });
